6cf045c4fda222fa496eab014cd85b0ebdc80d30
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-01 23:23:30 +0600

    Arrow Functions

---
+++ tut.js
@@ -0,0 +1,19 @@
+// Normal Function in JS
+export default function doSomething() {
+  // function body
+}
+
+// Arrow Function in JS
+export const doSomething = () => {
+  // function body
+};
+
+const MyComponent = () => {
+  return <div></div>;
+};
+
+<button
+  onClick={() => {
+    console.log("hello world");
+  }}
+></button>;
